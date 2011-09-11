325f07372c0515377b8574382af5bb092b79b5e9
meh <meh@paranoici.org>
2011-09-11 13:07:34 +0200

    Added write support

--- lib/ffi/extra.rb
+++ lib/ffi/extra.rb
@@ -10,122 +10,143 @@
 #  0. You just DO WHAT THE FUCK YOU WANT TO.
 #++
 
-class Integer
-  def to_ffi
-    self
-  end
-end
-
-class String
-  def to_ffi
-    self
-  end
-end
-
-class NilClass
-  def to_ffi
-    self
-  end
-end
-
 module FFI
-  def self.type_size (type)
-    type = FFI.find_type(type) if type.is_a?(Symbol)
+	def self.type_size (type)
+		type = FFI.find_type(type) if type.is_a?(Symbol)
 
-    if type.is_a?(Type::Builtin) || type.is_a?(Class) && type.ancestors.member?(FFI::Struct) || type.ancestors.member?(FFI::ManagedStruct)
-      type.size
-    elsif type.respond_to? :from_native
-      type.native_type.size
-    else
-      raise ArgumentError, 'you have to pass a Struct, a Builtin type or a Symbol'
-    end
-  end
+		if type.is_a?(Type::Builtin) || type.is_a?(Class) && type.ancestors.member?(FFI::Struct) || type.ancestors.member?(FFI::ManagedStruct)
+			type.size
+		elsif type.respond_to? :from_native
+			type.native_type.size
+		else
+			raise ArgumentError, 'you have to pass a Struct, a Builtin type or a Symbol'
+		end
+	end
 
-  module Library
-    def ffi_lib_add (*names)
-      ffi_lib *((begin
-        ffi_libraries
-      rescue Exception
-        []
-      end).map {|lib|
-        lib.name
-      } + names).compact.uniq.reject {|lib|
-        lib == '[current process]'
-      }
-    end
+	module Library
+		def ffi_lib_add (*names)
+			ffi_lib *((begin
+				ffi_libraries
+			rescue Exception
+				[]
+			end).map {|lib|
+				lib.name
+			} + names).compact.uniq.reject {|lib|
+				lib == '[current process]'
+			}
+		end
 
-    def has_function? (sym, libraries=nil)
-      libraries ||= ffi_libraries
+		def has_function? (sym, libraries=nil)
+			libraries ||= ffi_libraries
 
-      libraries.any? {|lib|
-        if lib.is_a?(DynamicLibrary)
-          lib
-        else
-          DynamicLibrary.new(lib, 0)
-        end.find_function(sym.to_s) rescue nil
-      }
-    end
+			libraries.any? {|lib|
+				if lib.is_a?(DynamicLibrary)
+					lib
+				else
+					DynamicLibrary.new(lib, 0)
+				end.find_function(sym.to_s) rescue nil
+			}
+		end
 
-    def attach_function! (*args, &block)
-      begin
-        attach_function(*args, &block)
-      rescue Exception => e
-        false
-      end
-    end
-  end
+		def attach_function! (*args, &block)
+			begin
+				attach_function(*args, &block)
+			rescue Exception => e
+				false
+			end
+		end
+	end
 
-  class Type::Builtin
-    def name
-      inspect[/:(\w+) /][1 .. -2]
-    end
-  end
+	class Type::Builtin
+		def name
+			inspect[/:(\w+) /][1 .. -2]
+		end
+	end
 
-  class Pointer
-    def typecast (type)
-      if type.is_a?(Symbol)
-        if respond_to? "read_#{type}"
-          return send "read_#{type}"
-        else
-          type = FFI.find_type(type)
-        end
-      end
+	class Pointer
+		def read (type)
+			if type.is_a?(Symbol)
+				if respond_to? "read_#{type}"
+					return send "read_#{type}"
+				else
+					type = FFI.find_type(type)
+				end
+			end
 
-      if type.is_a?(Type::Builtin)
-        send "read_#{type.name.downcase}"
-      elsif type.is_a?(Class) && type.ancestors.member?(FFI::Struct) && !type.ancestors.member?(FFI::ManagedStruct)
-        type.new(self)
-      elsif type.respond_to? :from_native
-        type.from_native(typecast(type.native_type), nil)
-      else
-        raise ArgumentError, 'you have to pass a Struct, a Builtin type or a Symbol'
-      end
-    end
+			if type.is_a?(Type::Builtin)
+				send "read_#{type.name.downcase}"
+			elsif type.is_a?(Class) && type.ancestors.member?(FFI::Struct) && !type.ancestors.member?(FFI::ManagedStruct)
+				type.new(self)
+			elsif type.respond_to? :from_native
+				type.from_native(typecast(type.native_type), nil)
+			else
+				raise ArgumentError, 'you have to pass a Struct, a Builtin type or a Symbol'
+			end
+		end; alias typecast read
 
-    def read_array_of (type, number)
-      if type.is_a?(Symbol)
-        if respond_to? "read_array_of_#{type}"
-          return send "read_array_of_#{type}"
-        else
-          type = FFI.find_type(type)
-        end
-      end
+		def write (what, type=nil)
+			if type
+				if respond_to? "write_#{type.downcase}"
+					send "write_#{type.downcase}", what
+				else
+					write_bytes what, what.size
+				end
+			else
+				case what
+					when FFI::Struct then write_bytes what.pointer.read_bytes(what.size)
+					when String      then write_bytes what
+					else raise ArgumentError, 'I do not know how to deal with this variable'
+				end
+			end
+		end
 
-      type = type.native_type if type.respond_to? :native_type
+		def read_array_of (number, type)
+			if type.is_a?(Symbol)
+				if respond_to? "read_array_of_#{type.downcase}"
+					return send "read_array_of_#{type.downcase}", number
+				else
+					type = FFI.find_type(type)
+				end
+			end
 
-      if type.is_a?(Class) && type.ancestors.member?(FFI::Struct) || type.is_a?(FFI::ManagedStruct)
-        read_array_of_pointer(number).map {|pointer|
-          type.new(pointer)
-        }
-      else
-        begin
-          send "read_array_of_#{type.name.downcase}", number
-        rescue
-          raise ArgumentError, "#{type.name} is not supported"
-        end
-      end
-    end
-  end
+			type = type.native_type if type.respond_to? :native_type
 
-  find_type(:size_t) rescue typedef(:ulong, :size_t)
+			if type.is_a?(Class) && type.ancestors.member?(FFI::Struct)
+				read_array_of_pointer(number).map {|pointer|
+					type.new(pointer)
+				}
+			else
+				begin
+					send "read_array_of_#{type.name.downcase}", number
+				rescue NameError
+					raise ArgumentError, "#{type.name} is not supported"
+				end
+			end
+		end
+
+		def write_array_of (data, type)
+			if type.is_a?(Symbol)
+				if respond_to? "write_array_of_#{type}"
+					return send "write_array_of_#{type}", data
+				else
+					type = FFI.find_type(type)
+				end
+			end
+
+			type = type.native_type if type.respond_to? :native_type
+
+			if type.is_a?(Class) && type.ancestors.member?(FFI::Struct)
+				write_array_of_pointer(data)
+			else
+				begin
+					send "write_array_of_#{type.name.downcase}", data
+				rescue NameError
+					raise ArgumentError, "#{type.name} is not supported"
+				end
+			end
+		end
+	end
+
+	find_type(:size_t) rescue  typedef(:ulong, :size_t)
+	find_type(:ssize_t) rescue typedef(:long,  :ssize_t)
 end
